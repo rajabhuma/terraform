5383bc5b492f55f1514e3bf70a680e7e75feadf4
rajabhuma <rajabhuma@outlook.com>
2022-09-30 23:56:26 +0530

    initial changes

--- main.tf
+++ main.tf
@@ -11,3 +11,13 @@ provider "aws" {
   profile = "default"
   region  = "ap-south-1"
 }
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "test-bucket"
+    Environment = "Dev"
+  }
+}
